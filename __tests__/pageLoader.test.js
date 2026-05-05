63adffcee4cadce2e3b6d2c7698efcce6ce5665d
Dominika Vender <me@dven.dev>
2026-05-05 19:01:44 +0300

    add image downloading

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -27,9 +27,34 @@ test('скачивает страницу и возвращает путь', asy
     .catch(() => false);
 
   expect(exists).toBe(true);
-
   expect(path.basename(filePath)).toBe('ru-hexlet-io-courses.html');
 
   const content = await fs.promises.readFile(filePath, 'utf-8');
-  expect(content).toBe('<html>Hello</html>');
+  expect(content).toContain('Hello');
+});
+
+test('скачивает изображения и меняет ссылки в HTML', async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, '<html><img src="/assets/professions/nodejs.png" /></html>');
+
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, Buffer.from('fakepng'), { 'Content-Type': 'image/png' });
+
+  const filePath = await pageLoader('https://ru.hexlet.io/courses', tmpDir);
+
+  const content = await fs.promises.readFile(filePath, 'utf-8');
+  expect(content).toContain('ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png');
+
+  const imgPath = path.join(
+    tmpDir,
+    'ru-hexlet-io-courses_files',
+    'ru-hexlet-io-assets-professions-nodejs.png',
+  );
+  const imgExists = await fs.promises.access(imgPath)
+    .then(() => true)
+    .catch(() => false);
+
+  expect(imgExists).toBe(true);
 });
--- src/index.js
+++ src/index.js
@@ -1,21 +1,53 @@
 import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
+import * as cheerio from 'cheerio';
 
-const buildFileName = (url) => {
+const buildName = (url) => {
   const { hostname, pathname } = new URL(url);
   const raw = `${hostname}${pathname}`;
-  const name = raw.replace(/[^a-zA-Z0-9]/g, '-');
-  return `${name}.html`;
+  return raw.replace(/[^a-zA-Z0-9]/g, '-');
 };
 
-const pageLoader = (url, outputDir = process.cwd()) => {
-  const fileName = buildFileName(url);
-  const filePath = path.join(outputDir, fileName);
+const buildImageName = (pageUrl, src) => {
+  const { hostname } = new URL(pageUrl);
+  const ext = path.extname(src);
+  const nameWithoutExt = src.replace(ext, '');
+  const raw = `${hostname}${nameWithoutExt}`;
+  const name = raw.replace(/[^a-zA-Z0-9]/g, '-');
+  return `${name.slice(0, 150)}${ext}`;
+};
 
-  return axios.get(url)
-    .then((response) => fs.promises.writeFile(filePath, response.data))
-    .then(() => filePath);
+const pageLoader = async (url, outputDir = process.cwd()) => {
+  const baseName = buildName(url);
+  const htmlFile = path.join(outputDir, `${baseName}.html`);
+  const filesDir = `${baseName}_files`;
+  const filesDirPath = path.join(outputDir, filesDir);
+
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+
+  const images = [];
+  $('img').each((i, el) => {
+    const src = $(el).attr('src');
+    if (src && src.startsWith('/')) {
+      images.push({ el, src });
+    }
+  });
+
+  await fs.promises.mkdir(filesDirPath, { recursive: true });
+
+  for (const { el, src } of images) {
+    const imgUrl = new URL(src, url).toString();
+    const imgName = buildImageName(url, src);
+    const imgPath = path.join(filesDirPath, imgName);
+    const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+    await fs.promises.writeFile(imgPath, imgResponse.data);
+    $(el).attr('src', `${filesDir}/${imgName}`);
+  }
+
+  await fs.promises.writeFile(htmlFile, $.html());
+  return htmlFile;
 };
 
 export default pageLoader;
